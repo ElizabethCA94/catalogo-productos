df461dd8aca1d5aac6a58c20105b6ece4c38a1b2
Daniel Santiago Gaviria Galvis <92945556+dgaviriag@users.noreply.github.com>
2022-01-13 20:42:00 -0500

    Se pone el estilo para el login y cuando se envía el formulario se dirige hacia index.php

--- login.php
+++ login.php
@@ -1,18 +1,38 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charser="UTF-8">
-    <meta name="viewport" content="widtch-device-width, initial-scale-1.0">
-    <title>LOGIN</title>
-    
-</head>
+<?php include("db.php"); ?>
+<?php include("includes/header_footer.php"); ?>
 <body>
-    <form action="validarlogin.php" method="POST">
-        <h3>Sistema de login</h3>
-        <p>Usuario <input type="text" placeholder="Ingrese su usuario" name="usuario"></p>
-        <p>Contraseña <input type="password" placeholder="Ingrese su contraseña" name="contraseña"></p>
-        <input type="submit" value="Ingresar">
-    </form>
-
+    <style>
+        #formularioLog{
+            width: 40%;
+            background-color: rgb(223, 171, 75);
+            margin-top: 5%;
+        }
+    </style>
+   
+    <div class="container">
+        <div class= "card mx-auto" id="formularioLog">
+            <div class="card-header">
+                <h3> Inicio de sesión </h3>
+            </div>
+                <div class="card-body">
+                    <form action="validarlogin.php" method="POST">
+                        <div class="mb-3">
+                            <p>Usuario <input type="email" class= "form-control" placeholder="Ingrese su usuario" name="usuario" aria-describedby="emailHelp"></p>
+                            <div id="emailHelp" class="form-text">No compartiremos tu informacion con nadie más</div>
+                        </div>
+                        <div class="mb-3">
+                            <p>Contraseña<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ingrese su contraseña" name="contraseña"></p>
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Mantener sesión iniciada</label>
+                        </div>
+                        <a href = "index.php"><button  type="submit" class="btn btn-success" >Enviar</button></a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
