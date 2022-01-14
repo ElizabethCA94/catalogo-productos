c16e2bed06b8fa4628f12bcab225e20c2e6315a6
mariacamila613 <76500094+mariacamila613@users.noreply.github.com>
2022-01-13 19:25:39 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charser="UTF-8">
+    <meta name="viewport" content="widtch-device-width, initial-scale-1.0">
+    <title>LOGIN</title>
+    
+</head>
+<body>
+    <form action="validarlogin.php" method="POST">
+        <h3>Sistema de login</h3>
+        <p>Usuario <input type="text" placeholder="Ingrese su usuario" name="usuario"></p>
+        <p>Contraseña <input type="password" placeholder="Ingrese su contraseña" name="contraseña"></p>
+        <input type="submit" value="Ingresar">
+    </form>
+
+</body>
+</html>
